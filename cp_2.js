0039d3ba3726d0abc17e3932259e7318121bacb1
Justin Wong <jcwong5@usf.edu>
2025-10-17 13:54:17 -0400

    Made the displayProducts function that loops through the first 5 products from the API

--- cp_2.js
+++ cp_2.js
@@ -21,3 +21,19 @@ async function fetchProductsAsync() {
         return null
     }
 }
+
+function displayProducts(products) {
+    container.innerhtml= "";
+    product.slice(0,5).forEach((p)=>{const{name, price}=p.field; 
+const imageUrl = getImageUrl(p.field);
+const card = document.createElement("div")
+card.classname = "Product-card";
+card.innerHTML=`
+<img class="product-image" src="${imageUrl}" alt="${escapeHTML(name)}">
+<div class="product-name">${escapeHTML(name)}</div>
+<div class="product-price">$${(price/100).toFixed(2)}</div>
+`;
+container.appendChild(card);
+});
+statusEl.textContent = "Here are the Products"
+}
